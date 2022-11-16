241797769706e5f769a52d36d6d5a28331032101
CalSch <cal.schwick@gmail.com>
2022-11-16 18:16:51 -0500

    Finished

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+let frameDoc=document.querySelector('iframe').contentDocument;
+let oldVal="";
+
 setInterval(()=>{
-	
+	if (!editor) return;
+	let val=editor.getValue()
+	if (val!==oldVal) {
+		oldVal=val;
+		frameDoc.querySelector('style').innerHTML=val;
+	}
 },1000/10)
